27e1aa0d37f055a7ec6fbe76549e0564da031c36
dch <dch@merzbp.local>
2022-10-28 13:45:09 +0100

    arg parsing as function

--- ipod_extract.py
+++ ipod_extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Extract and rename iPod audio data to local drive"""
 
+import sys
 import argparse
 import os
 import logging
@@ -9,30 +10,42 @@ from tinytag import TinyTag
 from tqdm import tqdm
 
 
-LOG = './ipod_extract.log'
-home = os.path.expanduser('~')
+LOG = "./ipod_extract.log"
+home = os.path.expanduser("~")
 extensions = [".mp3", ".m4a", ".mp4", ".m4b", ".aac", ".aiff"]
 various = ["VA", "Various", "Various Artists"]
 
-logging.basicConfig(filename=LOG,level=logging.DEBUG,format='%(asctime)s %(message)s', \
-    datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(
+    filename=LOG,
+    level=logging.DEBUG,
+    format="%(asctime)s %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S",
+)
 
-parser = argparse.ArgumentParser(
-    description="Extract and rename iPod audio data to local drive")
-parser.add_argument("-i", type=str, required=True,
-                    help='path to mounted iPod')
-parser.add_argument("-o", type=str,
-                    help='path to local directory for extraction (default: ~/iPod/Music)')
-args = parser.parse_args()
+
+def parse_args(args):
+    """Define and parse args"""
+    parser = argparse.ArgumentParser(
+        description="Extract and rename iPod audio data to local drive"
+    )
+    parser.add_argument("-i", type=str, required=True, help="path to mounted iPod")
+    parser.add_argument(
+        "-o",
+        type=str,
+        help="path to local directory for extraction (default: ~/iPod/Music)",
+    )
+    return parser.parse_args()
+
 
 def cleanse(val):
     """Sanitize tag strings for dir/filenames"""
-    val = val.replace('/', '').replace(':', '-').replace('?', '')
+    val = val.replace("/", "").replace(":", "-").replace("?", "")
     return val
+
 
 def pad_zeroes(track):
     """Pad zeros on track numbers < 10"""
-    if track[0].isdigit() and track[0] != '0':
+    if track[0].isdigit() and track[0] != "0":
         if int(track) in range(0, 10):
             padded = f"0{track}"
             return padded
@@ -42,31 +55,42 @@ def pad_zeroes(track):
 def iter_files(ipod_path):
     """Walk iPod dir path and pass files to parser"""
     for root, dirs, files in os.walk(ipod_path, topdown=False):
-        for filename in tqdm(files, colour='green', desc=f'{os.path.basename(root)}...'):
-            if filename.endswith(tuple(extensions)) and not filename.startswith('._'):
+        for filename in tqdm(
+            files, colour="green", desc=f"{os.path.basename(root)}..."
+        ):
+            if filename.endswith(tuple(extensions)) and not filename.startswith("._"):
                 audio_file = os.path.join(root, filename)
                 ext = Path(audio_file).suffix
                 try:
-                    tags = TinyTag.get(f'{audio_file}')
-                    extract_file(audio_file,tags,ext)
+                    tags = TinyTag.get(f"{audio_file}")
+                    extract_file(audio_file, tags, ext)
                 except Exception as error:
                     msg = f"Parsing `{audio_file}` failed: {error}"
+                    unparsed_dump = (
+                        f"{str(args.o)}" if args.o else f"{home}/iPod/Unknown_Tags/"
+                    )
+                    os.system(f"cp {audio_file} {unparsed_dump}")
                     tqdm.write(msg)
                     logging.info(msg)
 
 
-def extract_file(file,tags,ext):
+def extract_file(file, tags, ext):
     """Make directory for audio files, copy files into proper dirs, rename files"""
-    artist = 'Various' if tags.albumartist in various else cleanse(tags.artist)
+    artist = "Various" if tags.albumartist in various else cleanse(tags.artist)
     album = cleanse(tags.album)
-    album_path = f'{str(args.o)}/{artist}/{album}/' if args.o\
-         else f'{home}/iPod/Music/{artist}/{album}/'
-    new_filename = f'{pad_zeroes(tags.track)} - {cleanse(tags.artist)} - {cleanse(tags.title)}{ext}'
+    album_path = (
+        f"{str(args.o)}/{artist}/{album}/"
+        if args.o
+        else f"{home}/iPod/Music/{artist}/{album}/"
+    )
+    new_filename = f"{pad_zeroes(tags.track)} - {cleanse(tags.artist)} - {cleanse(tags.title)}{ext}"
     try:
-        os.makedirs(f'{album_path}', exist_ok=True)
+        os.makedirs(f"{album_path}", exist_ok=True)
         os.system(f'cp "{file}" "{album_path}"')
-        os.system(f'mv "{album_path}/{os.path.split(file)[1]}" "{album_path}/{new_filename}"')
-        msg = f'Copied {file} to {album_path}{new_filename}'
+        os.system(
+            f'mv "{album_path}/{os.path.split(file)[1]}" "{album_path}/{new_filename}"'
+        )
+        msg = f"Copied {file} to {album_path}{new_filename}"
         tqdm.write(msg)
         logging.info(msg)
     except Exception as error:
@@ -76,5 +100,6 @@ def extract_file(file,tags,ext):
 
 
 if __name__ == "__main__":
+    args = parse_args(sys.argv[:1])
     ipod_path = args.i
     iter_files(ipod_path)
