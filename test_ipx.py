2d79321c9c6cf53c10b3d33cd12acfc92933d9f9
CD <dch@dchair.local>
2022-11-06 03:10:10 +0000

    add test_pad_zeroes

--- test_ipx.py
+++ test_ipx.py
@@ -3,7 +3,7 @@ import pytest
 import ipx
 
 def test_cleanse():
-    """Ensure """
+    """Test cleanse func"""
     illegal_chars = ['/','\\','<','>','*','?','|',':']
     test_vals = [
         'Band1 / Band2',
@@ -19,3 +19,12 @@ def test_cleanse():
             assert cleansed == 'The Album- Special Edition'
         for c in cleansed:
             assert c not in illegal_chars 
+
+def test_pad_zeroes():
+    """Test pad_zeroes func"""
+    test_tracks = ['1','42', '7', '333']
+    for track in test_tracks:
+        if len(track) < 2:
+            assert ipx.pad_zeroes(track) == f'0{track}'
+        else:
+            assert ipx.pad_zeroes(track) == track
